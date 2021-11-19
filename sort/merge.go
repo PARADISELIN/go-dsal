a1089e40dbafd8be295a852499884705b04dfe13
linlei <lin92731@outlook.com>
2021-11-19 13:08:03 +0800

    feat: add merge sort

---
+++ sort/merge.go
@@ -0,0 +1,43 @@
+package sort
+
+// 合并两个有序的区间 arr[l, mid] 和 arr[mid + 1, r]
+// 注意这里是闭区间
+func merge(arr []int, l int, mid int, r int) {
+	temp := make([]int, r - l + 1)
+	copy(temp, arr[l:r + 1])
+
+	i := l
+	j := mid + 1
+
+	// 每轮循环为arr[k]赋值
+	for k := l; k <= r; k++ {
+		if i > mid {
+			arr[k] = temp[j - l]
+			j++
+		} else if j > r {
+			arr[k] = temp[i - l]
+			i++
+		} else if temp[i - l] <= temp[j - l] {
+			arr[k] = temp[i - l]
+			i++
+		} else {
+			arr[k] = temp[j - l]
+			j++
+		}
+	}
+}
+
+func sort(arr []int, l int, r int) {
+	if l >= r {
+		return
+	}
+
+	mid := l + (r - l) / 2
+	sort(arr, l , mid)
+	sort(arr, mid + 1, r)
+	merge(arr, l , mid, r)
+}
+
+func MergeSort(arr []int) {
+	sort(arr, 0, len(arr) - 1)
+}
