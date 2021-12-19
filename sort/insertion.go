53a92f9b4b3ceaa0154a9a0891cafcecaa71affa
linlei <lin92731@outlook.com>
2021-12-19 14:41:02 +0800

    refoctor: update insertion sort

--- sort/insertion.go
+++ sort/insertion.go
@@ -2,6 +2,18 @@ package sort
 
 func InsertionSort(arr []int) {
 	for i := 0; i < len(arr); i++ {
+		for j := i; j - 1 >= 0; j-- {
+			if arr[j] < arr[j - 1] {
+				arr[j], arr[j - 1] = arr[j - 1], arr[j]
+			} else {
+				break
+			}
+		}
+	}
+}
+
+func insertionSortOptimized1(arr []int) {
+	for i := 0; i < len(arr); i++ {
 		for j := i; j - 1 >= 0 && arr[j] < arr[j - 1]; j-- {
 			arr[j], arr[j - 1] = arr[j - 1], arr[j]
 		}
@@ -9,7 +21,7 @@ func InsertionSort(arr []int) {
 }
 
 // optimization: reduce the cost of each exchange operation
-func insertionSortOptimized(arr []int) {
+func insertionSortOptimized2(arr []int) {
 	for i := 0; i < len(arr); i++ {
 		cur := arr[i]
 		var j int
